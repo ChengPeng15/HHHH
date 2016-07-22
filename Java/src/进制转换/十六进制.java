2f5c198eebc1ad6a46f82c9a8fa92d0a8c771151
chengpeng15 <15126118@bjtu.edu.cn>
2016-07-22 11:52:23 +0800

    update

--- Java/src/进制转换/十六进制.java
+++ Java/src/进制转换/十六进制.java
@@ -9,6 +9,11 @@ public class 十六进制 {
 		System.out.println(Integer.toOctalString(a));//8进制
 		System.out.println(a);//16*16*16=4096
 		System.out.println(Integer.parseInt("1000",16));
+		System.out.println(Integer.toBinaryString(10));//10转为2进制
+		System.out.println(Integer.toBinaryString(0xa));//10转为2进制
+		System.out.println(Integer.toBinaryString(0xA));//10转为2进制
+		System.out.println(0xA);
+		System.out.println(Integer.toHexString(14));//
 
 
 	}
