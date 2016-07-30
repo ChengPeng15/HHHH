f0e3cf06453d59fb48c54851e243408adca7709a
chengpeng15 <15126118@bjtu.edu.cn>
2016-07-30 22:19:27 +0800

    test

--- Java/src/数据类型/next和nextline区别.java
+++ Java/src/数据类型/next和nextline区别.java
@@ -19,14 +19,16 @@ import java.util.Scanner;public class NextTest
 public class next和nextline区别 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in); 
+
+		while(sc.hasNext()){
 			String s1,s2;  
-			Scanner sc=new Scanner(System.in); 
 			System.out.print("请输入第一个字符串："); 
-			s1=sc.nextLine(); 
+			s1=sc.nextLine(); //nextLine是都一行
 			System.out.print("请输入第二个字符串：");  
 			s2=sc.next(); 
-			System.out.println("输入的字符串是："+s1+"  "+s2);
-		
+			System.out.println("输入的字符串dd是："+s1+":"+s2);
+		}
 		/*
 		
 		String s1,s2;  
