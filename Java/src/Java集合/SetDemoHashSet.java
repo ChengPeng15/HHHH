9b3c633c3e5f0e116f390659cc2bd4c1e6e562fd
chengpeng15 <15126118@bjtu.edu.cn>
2016-08-06 09:50:11 +0800

    update

--- Java/src/Java集合/SetDemoHashSet.java
+++ Java/src/Java集合/SetDemoHashSet.java
@@ -1,6 +1,7 @@
 package Java集合;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class SetDemoHashSet {
@@ -10,11 +11,23 @@ public class SetDemoHashSet {
 		s = new HashSet<String>();//无序
 		s.add("M");
 		s.add("B");
-		s.add("D");
-		s.add("C");
+		System.out.println(s.add("D"));
+		System.out.println(s.add("D"));
 		s.add(null);
 		s.add("E");
 		s.add("F");
+		System.out.println(s);
+
+		
+		Iterator<String> iterList = s.iterator();
+		while(iterList.hasNext()){
+			String str = iterList.next();
+			if("A".equals(str)){
+				s.add("FF");//没用
+			}else{
+				System.out.println(str);
+			}
+		}
 		System.out.println(s);
 	}
 
