70085ec61f50a48f65df2607c03ed638f55a3410
chengpeng15 <15126118@bjtu.edu.cn>
2016-08-30 14:52:50 +0800

    算法

---
+++ Java/src/剑指offer/Transform.java
@@ -0,0 +1,30 @@
+package 剑指offer;
+
+/*
+ * 对于两个字符串A和B，如果A和B中出现的字符种类相同且每种字符出现的次数相同，则A和B互为变形词，请设计一个高效算法，检查两给定串是否互为变形词。
+
+给定两个字符串A和B及他们的长度，请返回一个bool值，代表他们是否互为变形词。
+
+测试样例：
+"abc",3,"bca",3
+返回：true
+ */
+public class Transform {
+    public boolean chkTransform(String str1, int a, String str2, int b) {
+        if (str1 == null || str2 == null || str1.length() != str2.length()) {
+            return false;
+        }
+        char[] chas1 = str1.toCharArray();
+        char[] chas2 = str2.toCharArray();
+        int[] map = new int[256];
+        for (int i = 0; i < chas1.length; i++) {
+            map[chas1[i]]++;
+        }
+        for (int i = 0; i < chas2.length; i++) {
+            if (map[chas2[i]]-- == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
