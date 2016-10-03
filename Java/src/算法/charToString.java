9d8888da168c9279af177c37e52c0bbcba8d584a
chengpeng <15126118@bjtu.edu.cn>
2016-10-03 16:18:04 +0800

    new String()方法的，学习下

---
+++ Java/src/算法/charToString.java
@@ -0,0 +1,12 @@
+package 算法;
+
+public class charToString {
+
+	public static void main(String[] args) {
+//		char[]array={'1','2','3','4','5'};
+		String str="Hello World";
+		char[]array=str.toCharArray();
+		System.out.println(new String(array));
+	}
+
+}
